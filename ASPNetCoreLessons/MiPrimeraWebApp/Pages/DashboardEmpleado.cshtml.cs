243d8463dc4a3080a43eddae97d92e2c23ca0abf
ChrisVoura <crissnchz983@gmail.com>
2026-04-08 17:53:23 -0600

    Version 10

--- ASPNetCoreLessons/MiPrimeraWebApp/Pages/DashboardEmpleado.cshtml.cs
+++ ASPNetCoreLessons/MiPrimeraWebApp/Pages/DashboardEmpleado.cshtml.cs
@@ -10,8 +10,11 @@ namespace MiPrimeraWebApp.Pages
 
         public int ProductosStockBajo { get; set; }
         public int PedidosRecientes { get; set; }
+        public int TotalProductos { get; set; }
+        public int ProductosEnDescuento { get; set; }
         public List<Producto> ProductosStockBajoLista { get; set; } = new();
         public List<Pedido> PedidosRecientesLista { get; set; } = new();
+        public List<Producto> ProductosEnDescuentoLista { get; set; } = new();
         public string UsuarioRol { get; set; } = "";
 
         public DashboardEmpleadoModel(AppDbContext db)
@@ -30,8 +33,10 @@ namespace MiPrimeraWebApp.Pages
             
             var fechaLimite = DateTime.Now.AddDays(-7);
             
+            TotalProductos = _db.Productos.Count();
             ProductosStockBajo = _db.Productos.Count(p => p.Stock < 10);
             PedidosRecientes = _db.Pedidos.Count(p => p.FechaPedido >= fechaLimite);
+            ProductosEnDescuento = _db.Productos.Count(p => p.DescuentoActivo);
             
             ProductosStockBajoLista = _db.Productos
                 .Where(p => p.Stock < 10)
@@ -41,6 +46,11 @@ namespace MiPrimeraWebApp.Pages
             PedidosRecientesLista = _db.Pedidos
                 .Where(p => p.FechaPedido >= fechaLimite)
                 .OrderByDescending(p => p.FechaPedido)
+                .ToList();
+
+            ProductosEnDescuentoLista = _db.Productos
+                .Where(p => p.DescuentoActivo)
+                .OrderByDescending(p => p.PorcentajeDescuento)
                 .ToList();
             
             return Page();

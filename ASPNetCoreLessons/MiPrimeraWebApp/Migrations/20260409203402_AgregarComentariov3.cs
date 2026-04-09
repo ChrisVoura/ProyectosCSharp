1d5879e49c04b113e334db82b9373e6ff4f5e467
ChrisVoura <crissnchz983@gmail.com>
2026-04-09 17:06:56 -0600

    Version 11

---
+++ ASPNetCoreLessons/MiPrimeraWebApp/Migrations/20260409203402_AgregarComentariov3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiPrimeraWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarComentariov3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comentarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    valor = table.Column<int>(type: "INTEGER", nullable: false),
+                    FechaCreaciacion = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentarios");
+        }
+    }
+}

24ed1a460862e8d488433cddc39bd74f8778b179
ChrisVoura <crissnchz983@gmail.com>
2026-05-12 20:00:36 -0600

    Cambio en la migraciones

--- FleetTrackAPI/Program.cs
+++ FleetTrackAPI/Program.cs
@@ -66,6 +66,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
-
+//Migraciones
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
